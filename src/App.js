3924b22cb94fe4e3a46000799a6640c2441590c5
Anuj Siwach <anujkumar142000@gmail.com>
2024-09-30 00:35:24 +0530

    Added Loader
    Loader added to the Site
    A big thanks you Harshana for adding animation to the logo
    @little_lemon on instagram

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import { AppThemeProvider } from "./ui/theme/AppThemeProvider";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { routes } from "./conts/routes";
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import Loader from './conts/Loader/Loader'; // Import the Loader component
 
 export const ScrollContext = createContext();
 
 function App() {
-  const [ position, setPosition ] = useState(0);
+  const [position, setPosition] = useState(0);
+  const [loading, setLoading] = useState(true); // State to handle loading
+
+  // Simulate loading effect for the app
+  useEffect(() => {
+    const handleLoad = () => {
+      // Start the timer
+      setTimeout(() => {
+        setLoading(false); // Hide loader after 4 seconds
+      }, 4000);
+    };
+
+    window.addEventListener('load', handleLoad);
+    return () => window.removeEventListener('load', handleLoad);
+  }, []);
 
   return (
     <ScrollContext.Provider value={{ position, setPosition }}>
-      <AppThemeProvider>
-        <Router>
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.component}
-              />
-            ))}
-          </Routes>
-        </Router>
-      </AppThemeProvider>
+      {loading ? (
+        <Loader /> // Show the loader while loading
+      ) : (
+        <AppThemeProvider>
+          <Router>
+            <Routes>
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.component}
+                />
+              ))}
+            </Routes>
+          </Router>
+        </AppThemeProvider>
+      )}
     </ScrollContext.Provider>
   );
 }
---
+++ src/conts/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.css"; // Link the CSS for styling
+
+const Loader = () => {
+  return (
+    <div id="loader">
+      <div className="loader-gif"></div>
+    </div>
+  );
+};
+
+export default Loader;
